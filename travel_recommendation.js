3cc23d14816a077bc11cd12f40d0d2c17d6be19e
AsfaltMaple <solentuna@gmail.com>
2024-05-23 07:14:29 -0400

    cont clean1

---
+++ travel_recommendation.js
@@ -0,0 +1,13 @@
+const btnSearch = document.getElementById('btnSearch');
+const btnReset = document.getElementById('btnReset');
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+function thankyou(){
+    alert('Thank you for contacting us!');
+    name ="";
+    email ="";
+    message="";
+}
