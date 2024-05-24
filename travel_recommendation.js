e654ce8e5b4c35f58a2033beb12ac2e4c50b2331
AsfaltMaple <solentuna@gmail.com>
2024-05-24 08:15:48 -0400

    temp7

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -26,13 +26,15 @@ function searchDestination() {
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-        const destination = data.destinations.countries.find(item => item.name.toLowerCase() === input);
-        console.log(data.destination);
+        //data.conditions.find(item => item.name.toLowerCase() === input);
+        const country = data.countries.find(item => item.name.toLowerCase() === input);
+        console.log(country);
         
         //(item => item.name.toLowerCase() === input);
-        if (destination) {
+        if (country) {
            // const countryname = destination.countries.name.join(', ');
-            const countrycitiesname = destination.countries.cities.name.join(', ');
+            const countrycitiesname = data.countries.cities.join(', ');
+            console.log(countrycitiesname);
             const countrycitiesimg = destination.countries.cities.imageUrl.join(', ');
             const countrycitiesdesc = destination.countries.cities.description.join(', ');
             
@@ -41,9 +43,9 @@ function searchDestination() {
             resultDiv.innerHTML += `<h2>${destination.country}</h2>`;
             //resultDiv.innerHTML += `<img src="${destination.imagesrc}" alt="hjh">`;
   
-            resultDiv.innerHTML += `<p><strong>Countries:</strong> ${countrycitiesname}</p>`;
-            resultDiv.innerHTML += `<p><strong>Temples:</strong> ${temples}</p>`;
-            resultDiv.innerHTML += `<p><strong>Beaches:</strong> ${beaches}</p>`;
+            resultDiv.innerHTML += `<p><strong>Countries:</strong> ${country}</p>`;
+            resultDiv.innerHTML += `<p><strong>Temples:</strong> ${countrycitiesimg}</p>`;
+            resultDiv.innerHTML += `<p><strong>Beaches:</strong> ${countrycitiesdesc}</p>`;
           } else {
             resultDiv.innerHTML = 'Condition not found.';
           }
